600c52fe498ffeff469e2edb125e510723677e53
Min Le <122418348+minhducle25@users.noreply.github.com>
2026-04-30 11:51:40 +1000

    Update version and refactor m3u8 handling

--- plugins/nguonc_plugin.js
+++ plugins/nguonc_plugin.js
@@ -6,7 +6,7 @@ function getManifest() {
     return JSON.stringify({
         "id": "nguonc",
         "name": "Phim NguonC",
-        "version": "1.2.0",
+        "version": "1.3.0",
         "baseUrl": "https://phim.nguonc.com",
         "iconUrl": "https://raw.githubusercontent.com/youngbi/repo/main/plugins/nguonC.png",
         "isEnabled": true,
@@ -219,7 +219,7 @@ function parseMovieDetail(apiResponseJson) {
 
                         // Use Embed URL as ID to allow scraping Referer/M3u8 details
                         // If no embed, use m3u8 directly.
-                        var link = m3u8 || embed;
+                        var link = embed || m3u8;
 
                         if (link) {
                             episodes.push({
@@ -282,77 +282,46 @@ function parseMovieDetail(apiResponseJson) {
 }
 
 
-function parseDetailResponse(html) {
+function parseDetailResponse(html, fallbackUrl) {
     try {
-        // Find m3u8 link in the Embed HTML (JWPlayer/Source)
+        // Try to extract direct m3u8 from embed HTML
         var m3u8Regex = /file:\s*["']([^"']+\.m3u8[^"']*)["']|source:\s*["']([^"']+\.m3u8[^"']*)["']|src:\s*["']([^"']+\.m3u8[^"']*)["']|["']([^"']+\.m3u8[^"']*)["']/;
         var match = html.match(m3u8Regex);
+        var m3u8 = match ? (match[1] || match[2] || match[3] || match[4]) : "";
 
-        var m3u8 = "";
-        if (match) {
-            m3u8 = match[1] || match[2] || match[3] || match[4];
-        }
+        // Use direct m3u8 if found, otherwise fall back to embed URL (WebView)
+        var streamUrl = m3u8 || fallbackUrl || "";
 
-        // JS injected into WebView to disable JWPlayer advertising before it initializes
-        var adBlockJs = [
-            // 1. Block VAST/ad XHR requests (githubusercontent = VAST config, 6789x = ad click-out)
-            "(function(){",
-            "var _xhrOpen = XMLHttpRequest.prototype.open;",
-            "XMLHttpRequest.prototype.open = function(m,u){",
-            "  if(u && (u.indexOf('githubusercontent.com/hiller')!==-1 || u.indexOf('6789x.site')!==-1 || u.indexOf('streamc.xyz/1.mp4')!==-1)){",
-            "    this._blocked=true; return;",
-            "  }",
-            "  return _xhrOpen.apply(this,arguments);",
-            "};",
-            "var _xhrSend = XMLHttpRequest.prototype.send;",
-            "XMLHttpRequest.prototype.send = function(d){",
-            "  if(this._blocked) return;",
-            "  return _xhrSend.apply(this,arguments);",
-            "};",
-            // 2. Block via fetch API as well
-            "var _fetch = window.fetch;",
-            "window.fetch = function(u,o){",
-            "  var url = (typeof u==='string')?u:(u&&u.url)||'';",
-            "  if(url.indexOf('githubusercontent.com/hiller')!==-1 || url.indexOf('6789x.site')!==-1 || url.indexOf('streamc.xyz/1.mp4')!==-1){",
-            "    return Promise.resolve(new Response('',{status:200}));",
-            "  }",
-            "  return _fetch.apply(this,arguments);",
-            "};",
-            // 3. Override jwplayer setup to strip advertising config
-            "var _t=setInterval(function(){",
-            "  if(window.jwplayer){",
-            "    clearInterval(_t);",
-            "    var _orig=window.jwplayer;",
-            "    var _wrap=function(){",
-            "      var p=_orig.apply(this,arguments);",
-            "      if(p&&p.setup){",
-            "        var _os=p.setup.bind(p);",
-            "        p.setup=function(c){",
-            "          if(c){delete c.advertising; delete c.vast;}",
-            "          return _os(c);",
-            "        };",
-            "      }",
-            "      return p;",
-            "    };",
-            "    Object.keys(_orig).forEach(function(k){_wrap[k]=_orig[k];});",
-            "    window.jwplayer=_wrap;",
-            "  }",
-            "},30);",
-            "})();"
-        ].join("");
+        // Inject into WebView to block VAST ad requests and strip jwplayer advertising config
+        var customJs = "(function(){" +
+            "var xo=XMLHttpRequest.prototype.open;" +
+            "XMLHttpRequest.prototype.open=function(m,u){" +
+            "if(u&&(u.indexOf('githubusercontent.com/hiller')>-1||u.indexOf('6789x.site')>-1||u.indexOf('streamc.xyz/1.mp4')>-1)){this._b=1;return;}" +
+            "return xo.apply(this,arguments);};" +
+            "var xs=XMLHttpRequest.prototype.send;" +
+            "XMLHttpRequest.prototype.send=function(d){if(this._b)return;return xs.apply(this,arguments);};" +
+            "var of=window.fetch;" +
+            "if(of)window.fetch=function(u,o){var s=typeof u==='string'?u:'';" +
+            "if(s.indexOf('githubusercontent.com/hiller')>-1||s.indexOf('6789x.site')>-1||s.indexOf('streamc.xyz/1.mp4')>-1)" +
+            "return Promise.resolve(new Response('',{status:200}));" +
+            "return of.apply(this,arguments);};" +
+            "var t=setInterval(function(){" +
+            "if(window.jwplayer){clearInterval(t);" +
+            "var orig=window.jwplayer;" +
+            "window.jwplayer=function(){var p=orig.apply(this,arguments);" +
+            "if(p&&p.setup){var s=p.setup;" +
+            "p.setup=function(c){if(c){delete c.advertising;delete c.vast;}return s.call(p,c);};}return p;};" +
+            "Object.keys(orig).forEach(function(k){window.jwplayer[k]=orig[k];});}},20);" +
+            "})();";
 
-        if (m3u8) {
-            return JSON.stringify({
-                url: m3u8,
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                    "Referer": "https://embed.streamc.xyz/",
-                    "Custom-Js": adBlockJs
-                }
-            });
-        }
-
-        return "{}";
+        return JSON.stringify({
+            url: streamUrl,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "Referer": "https://embed.streamc.xyz/",
+                "Custom-Js": customJs
+            }
+        });
     } catch (error) { return "{}"; }
 }
 
